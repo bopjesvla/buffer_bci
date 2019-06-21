1ad4390c2cab138c63fc9dd3b7f22d3e3f8cd27d
Robin <robin.deheer@gmail.com>
2019-06-21 15:52:28 +0200

    Documented calibration files

--- P300roject/p300CalibrateSignals.m
+++ P300roject/p300CalibrateSignals.m
@@ -1,33 +1,45 @@
-try; cd(fileparts(mfilename('fullpath')));catch; end;
-try;
+% This script sets up a buffer to save and record data for 600ms after
+% every 'stimulus.rowtgtFlash' and 'stimulus.coltgtFlash' events and
+% terminates upon receiving a 'stimulus.training' event with value 'end'.
+% The recorded data is then saved to 'calibration_data.mat'.
+
+% Setting paths and connecting to the buffer
+try cd(fileparts(mfilename('fullpath'))); catch; end
+try
    run ../matlab/utilities/initPaths.m
 catch
-   msgbox({'Please change to the directory where this file is saved before running the rest of this code'},'Change directory'); 
+   msgbox({'Please change to the directory where this file is saved'... 
+       'before running the rest of this code'}, 'Change directory'); 
 end
-try; cd(fileparts(mfilename('fullpath')));catch; end; %ARGH! fix bug with paths on Octave
+try cd(fileparts(mfilename('fullpath'))); catch; end
 
-buffhost='localhost';buffport=1972;
-% wait for the buffer to return valid header information
+buffhost = 'localhost'; buffport = 1972;
+% Wait for the buffer to return valid header information
 hdr=[];
-while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) % wait for the buffer to contain valid data
+while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) )
   try 
-    hdr=buffer('get_hdr',[],buffhost,buffport); 
+    hdr=buffer('get_hdr', [], buffhost,buffport); 
   catch
     hdr=[];
     fprintf('Invalid header info... waiting.\n');
-  end;
+  end
   pause(1);
-end;
+end
 
-% set the real-time-clock to use
+% Set the real-time-clock to use
 initgetwTime;
 initsleepSec;
 
+% Record and save the data
 verb=1;
 trlen_ms=600;
-dname='calibrate_dataZina30';
+dname='calibration_data';
 
-[data,devents,state]=buffer_waitData(buffhost,buffport,[],'startSet',{{'stimulus.rowtgtFlash', 'stimulus.coltgtFlash'}},'exitSet',{'stimulus.training' 'end'},'verb',verb,'trlen_ms',trlen_ms);
-mi=matchEvents(devents,'stimulus.training','end'); devents(mi)=[]; data(mi)=[]; % remove the exit event
-fprintf('Saving %d epochs to : %s\n',numel(devents),dname);
-save(dname,'data','devents');
+[data,devents,state] = buffer_waitData(buffhost ,buffport, [],...
+    'startSet', {{'stimulus.rowtgtFlash', 'stimulus.coltgtFlash'}},...
+    'exitSet', {'stimulus.training' 'end'},...
+    'verb', verb,'trlen_ms', trlen_ms);
+mi=matchEvents(devents, 'stimulus.training', 'end'); devents(mi)=[]; 
+data(mi)=[]; % Remove the exit event
+fprintf('Saving %d epochs to : %s\n', numel(devents), dname);
+save(dname, 'data', 'devents');
--- P300roject/p300CalibrateStimulus.m
+++ P300roject/p300CalibrateStimulus.m
@@ -1,115 +1,102 @@
-try; cd(fileparts(mfilename('fullpath')));catch; end;
-try;
+% This script manages the window and the stimuli during the calibration 
+% phase. It also connects to the buffer to send events in order to indicate
+% what is happening on the screen. Parameters are configurable to modify
+% the experiment. 
+
+% Setting paths and connecting to the buffer
+try cd(fileparts(mfilename('fullpath'))); catch; end
+try
    run ../matlab/utilities/initPaths.m
 catch
-   msgbox({'Please change to the directory where this file is saved before running the rest of this code'},'Change directory'); 
+   msgbox({'Please change to the directory where this file is saved'...
+       'before running the rest of this code'}, 'Change directory'); 
 end
-try; cd(fileparts(mfilename('fullpath')));catch; end; %ARGH! fix bug with paths on Octave
+try cd(fileparts(mfilename('fullpath'))); catch; end 
 
-buffhost='localhost';buffport=1972;
-trigsocket=javaObject('java.net.DatagramSocket');
-trigsocket.connect(javaObject('java.net.InetSocketAddress','localhost',8300));
-% wait for the buffer to return valid header information
+buffhost = 'localhost'; buffport = 1972;
+% Wait for the buffer to return valid header information
 hdr=[];
-while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) % wait for the buffer to contain valid data
+while ( isempty(hdr) || ~isstruct(hdr) || (hdr.nchans==0) ) 
   try 
-    hdr=buffer('get_hdr',[],buffhost,buffport); 
+    hdr=buffer('get_hdr', [], buffhost, buffport); 
   catch
     hdr=[];
     fprintf('Invalid header info... waiting.\n');
-  end;
+  end
   pause(1);
-end;
+end
 
-% set the real-time-clock to use
+% Set the real-time-clock to use
 initgetwTime;
 initsleepSec;
 
-verb=0;
-nSeq=30;
-% nRepetitions=5;  % the number of complete row/col stimulus before sequence is finished
-cueDuration=2;
-stimDuration=.2; % the length a row/col is highlighted
-interSeqDuration=2;
-bgColor=[.5 .5 .5]; % background color (grey)
-flashColor=[1 1 1]; % the 'flash' color (white)
-tgtColor=[0 1 0]; % the target indication color (green)
+% CONFIGURABLE PARAMETERS
+verb = 1;               % Verb = 1 for debug information
+nSeq = 30;          	% Amount of sequences. Multiple of 15
+cueDuration = 2;        % Time in seconds a cue is displayed
+afterCueDuration = 1;   % Time in seconds after a cue is displayed
+stimDuration = .2;      % Time in seconds a row/col is highlighted
+interSeqDuration = 2;   % Time in seconds after 1 sequence of stimulation
+bgColor = [.5 .5 .5];   % Background color (grey)
+flashColor = [1 1 1];   % Flash color (white)
+tgtColor = [0 1 0];     % Target indication color (green)
 
-% the set of options the user will pick from
-% symbols={'A' 'B'; 'C' 'D'};
-symbols={'pause', 'up', 'tvOff', 'tv1', 'food'; 'left', 'down', 'right', 'tv2','toilet'; 'call1','call2','call3', 'tv3', 'pain'};
-numbers = [1 4 7 10 13; 2 5 8 11 14; 3 6 9 12 15];
+% The grid in textual and numerical representation
+symbols={'pause', 'up', 'tvOff', 'tv1', 'food';...
+         'left', 'down', 'right', 'tv2','toilet';...
+         'call1','call2','call3', 'tv3', 'pain'};
+numbers = reshape(1:15, [3 5]);
 
-% make the stimulus
+% WINDOW INITIALIZATION
 clf;
-[h]=initGrid(symbols);
+[h] = initGrid(symbols);
 
-tgtSeq = repmat([1:numel(symbols)]',ceil(nSeq/numel(symbols)));
+% Setting and shuffling targets
+tgtSeq = repmat((1:numel(symbols))', ceil(nSeq/numel(symbols)));
 tgtSeq = tgtSeq(randperm(nSeq));
 
-flashseqsmall = [1 2 3 4 5 6 7 8];
-flashseq = [flashseqsmall flashseqsmall flashseqsmall];
+% Setting and shuffling flash sequences
+flashseq = repmat(1:8, [1 3]); 
+% 1:5 are columns, 6:8 are rows
 x = flashseq(randperm(length(flashseq)));
-% x=flashseqsmall;
 
-% play the stimulus
-% reset the cue and fixation point to indicate trial has finished  
-set(h(:),'color',[.5 .5 .5]);
-msg=msgbox({'Press OK to start'},'OK');while ishandle(msg); pause(.2); end;
-sendEvent('stimulus.training','start');
-for si=1:nSeq;
-
+% Start the calibration after pressing OK
+msg=msgbox({'Press OK to start'}, 'OK'); while ishandle(msg); pause(.2); end
+sendEvent('stimulus.training', 'start');
+for si = 1:nSeq % For every sequence
   sleepSec(interSeqDuration);
-  sendEvent('stimulus.sequence','start');
-  % show the subject cue where to attend
-  tgtIdx=tgtSeq(si);
+  sendEvent('stimulus.sequence', 'start');
+  tgtIdx = tgtSeq(si);
   set(h(tgtIdx),'color',tgtColor);
-  drawnow;% expose; % N.B. needs a full drawnow for some reason
-  sendEvent('stimulus.targetSymbol',symbols{tgtIdx});
-  fprintf('%d) tgt=%s : ',si,symbols{tgtSeq(si)}); % debug info
+  drawnow; % Show the cue
+  sendEvent('stimulus.targetSymbol', symbols{tgtIdx});
+  if verb; fprintf('%d) tgt=%s : ', si, symbols{tgtSeq(si)}); end
   sleepSec(cueDuration);  
-  set(h(:),'color',bgColor); % rest all symbols to background color
-  drawnow;
-  sleepSec(1); 
-  for ri=1:numel(x); % reps
-%     for ei=1:numel(symbols); % symbs
+  set(h(tgtIdx),'color',bgColor); 
+  drawnow; % Remove the cue
+  sleepSec(afterCueDuration); 
+  for ri = 1:numel(x) % For every repetition
       set(h(:),'color',bgColor);
-      if x(ri) > 5
+      if x(ri) > 5 % If a row should flash
         rowflashed = x(ri)-5;
         set(h(rowflashed,:),'color',flashColor);
-        drawnow;
-        ev=sendEvent('stimulus.rowflash',numbers(rowflashed,:)); % indicate this row is 'flashed'
-        sendEvent('stimulus.rowtgtFlash',ismember(tgtIdx, numbers(rowflashed,:)),ev.sample); % indicate 'target' flashs
-%         if ismember(tgtIdx, numbers(rowflashed,:))
-%             trigsocket.send(javaObject('java.net.DatagramPacket',int8([1 0]),1));
-%         end
-      else
+        drawnow; % Initiate row flash
+        ev = sendEvent('stimulus.rowflash', numbers(rowflashed, :)); 
+        sendEvent('stimulus.rowtgtFlash', ismember(tgtIdx, ...
+            numbers(rowflashed, :)), ev.sample); % True if target flashed
+      else % A column should flash
         colflashed = x(ri);
         set(h(:,colflashed),'color',flashColor);
-        drawnow;
+        drawnow; % Initiate column flash
         ev=sendEvent('stimulus.colflash',numbers(:,colflashed));
-        sendEvent('stimulus.coltgtFlash',ismember(tgtIdx, numbers(:,colflashed)),ev.sample); % indicate 'target' flashs
-%         if ismember(tgtIdx, numbers(:,colflashed))
-%             trigsocket.send(javaObject('java.net.DatagramPacket',int8([1 0]),1));
-%         end
+        sendEvent('stimulus.coltgtFlash',ismember(tgtIdx, ...
+            numbers(:,colflashed)),ev.sample); % True if target flashed
       end
-      
-%       flashIdx=ei;
-%       % flash
-
-      
       sleepSec(stimDuration);
-      % reset
       set(h(:),'color',bgColor);
-      drawnow;      
-%     end
+      drawnow; % Terminate flash
   end
-   
-  % reset the cue and fixation point to indicate trial has finished  
-  set(h(:),'color',bgColor);
-  drawnow;
   sendEvent('stimulus.sequence','end');
-  fprintf('\n');
-end % sequences
-% end training marker
+  if verb; fprintf('\n'); end
+end
 sendEvent('stimulus.training','end');
